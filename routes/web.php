3685728721c67ccdf59a8ddf895c5d8a16ccc152
Jose-Sierra082005 <programador082005@gmsil.com>
2026-05-30 20:35:38 -0400

    feat(auth): agregar interfaz de vista del login con diseño táctico militar

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta temporal para demostración del login
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
